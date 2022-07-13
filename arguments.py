ed1bad2603a6ba6601f97d92484727a4e316aacd
Donny Chan <chenyingfa1999@qq.com>
2022-07-13 20:18:01 +0000

    update

---
+++ arguments.py
@@ -0,0 +1,82 @@
+import argparse
+
+def get_parser():
+    p = argparse.ArgumentParser()
+    p.add_argument(
+        '-orc', '--oracle',
+        type=str,
+        help='select dataset, value = 300/600/1600, default = 300',
+        default='300')
+    p.add_argument(
+        '-exp', '--experiment_root',
+        type=str,
+        help='root where to store models, losses and accuracies',
+        default='./' + os.sep + 'model_save')
+    # 改了，原100
+    p.add_argument(
+        '-nep', '--epochs',
+        type=int,
+        help='number of epochs to train for',
+        default=1)
+    p.add_argument(
+        '-lr', '--learning_rate',
+        type=float,
+        help='learning rate for the model, default=0.001',
+        default=0.001)
+    # 学习率衰减用的
+    p.add_argument('-lrS', '--lr_scheduler_step',
+                        type=int,
+                        help='StepLR learning rate scheduler step, default=20',
+                        default=20)
+
+    p.add_argument('-lrG', '--lr_scheduler_gamma',
+                        type=float,
+                        help='StepLR learning rate scheduler gamma, default=0.5',
+                        default=0.5)
+    # 改了，原100
+    p.add_argument('-its', '--iterations',
+                        type=int,
+                        help='number of episodes per epoch, default=100',
+                        default=100)
+
+    p.add_argument('-cTr', '--classes_per_it_tr',
+                        type=int,
+                        help='number of random classes per episode for training, default=60',
+                        default=60)
+
+    p.add_argument('-nsTr', '--num_support_tr',
+                        type=int,
+                        help='number of samples per class to use as support for training, default=5',
+                        default=5)
+
+    p.add_argument('-nqTr', '--num_query_tr',
+                        type=int,
+                        help='number of samples per class to use as query for training, default=5',
+                        default=5)
+
+    p.add_argument('-cVa', '--classes_per_it_val',
+                        type=int,
+                        help='number of random classes per episode for validation, default=5',
+                        default=5)
+    # 改了，原5
+    p.add_argument('-nsVa', '--num_support_val',
+                        type=int,
+                        help='number of samples per class to use as support for validation, default=5',
+                        default=4)
+    # 改了，原15
+    p.add_argument('-nqVa', '--num_query_val',
+                        type=int,
+                        help='number of samples per class to use as query for validation, default=15',
+                        default=4)
+    # 用来搞随机数
+    p.add_argument('-seed', '--manual_seed',
+                        type=int,
+                        help='input for the manual seeds initializations',
+                        default=7)
+
+    p.add_argument('--cuda',
+                        type=bool,
+                        help='enables cuda',
+                        default=True)
+
+    return p
---
+++ train.py
@@ -0,0 +1,187 @@
+# import torch.nn.functional as F
+import torch
+import os
+from collections import Counter
+
+import numpy as np
+from tqdm import tqdm
+# from collections import *
+
+# from dataloader import *
+# from testModel import test_model
+from modeling.model import ProtoNet, prototypical_loss, euclidean_dist
+# from source.parser import get_parser
+# from source.sampler import PrototypicalBatchSampler
+from arguments import get_parser
+from sampler import PrototypicalBatchSampler
+
+# from torch.utils import data
+# from torch.utils.data import DataLoader
+from torchvision import datasets, transforms as T
+
+
+transform = T.Compose([
+    T.Resize([50, 50]),
+    # T.Grayscale(),
+    T.ToTensor(),
+    T.Normalize(mean=[.5, .5, .5], std=[.5, .5, .5])
+])
+
+
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+
+
+def mean(arrs):
+    return sum(arrs) * 1.0 / len(arrs)
+
+
+def init_seed(opt):
+    '''
+    Disable cudnn to maximize reproducibility
+    torch.cuda.cudnn_enabled = False
+    '''
+    # 如果每次运行代码时都设置相同的seed，则每次生成的随机数也相同
+    np.random.seed(opt.manual_seed)
+    torch.manual_seed(opt.manual_seed)
+    torch.cuda.manual_seed(opt.manual_seed)
+
+
+def init_dataloader(opt, mode):
+    # dataset
+    global n_classes
+    dataset = datasets.ImageFolder(
+        "./data_oct/" + mode + "_" + opt.oracle, transform=transform)
+    n_classes = len(np.unique([dataset[i][1] for i in range(len(dataset))]))
+    print("n_classes", mode, n_classes)  # 361
+    if n_classes < opt.classes_per_it_tr or n_classes < opt.classes_per_it_val:
+        raise (Exception('There are not enough classes in the dataset in order ' +
+                         'to satisfy the chosen classes_per_it. Decrease the ' +
+                         'classes_per_it_{tr/val} option and try again.'))
+    # sampler
+
+    if 'train' in mode:
+        classes_per_it = opt.classes_per_it_tr
+        num_samples = opt.num_support_tr + opt.num_query_tr
+    else:  # validation和test用同一组..？
+        classes_per_it = opt.classes_per_it_val
+        num_samples = opt.num_support_val + opt.num_query_val
+    labels = [dataset[i][1] for i in range(len(dataset))]
+    # print ( classes_per_it, num_samples )  # 60  , 10
+    # classes_per_it_tr number of random classes per episode for training, default=60'
+    # num_samples = opt.num_support_tr + opt.num_query_tr
+    # 一个batch 内的样本数也就是 classes_per_it_tr *  num_samples
+
+    batch_sampler = PrototypicalBatchSampler(labels=labels,
+                                             classes_per_it=classes_per_it,
+                                             num_samples=num_samples,
+                                             iterations=opt.iterations)
+    # dataloader
+    dataloader = torch.utils.data.DataLoader(
+        dataset, batch_sampler=batch_sampler)
+    # for e in dataloader:
+    #     #print (e )
+    #     asf
+    return dataloader
+
+
+def save_list_to_file(path, thelist):
+    with open(path, 'w') as f:
+        for item in thelist:
+            f.write("%s\n" % item)
+
+
+def train(opt, tr_dataloader, model, optim, lr_scheduler, val_dataloader=None):
+    '''
+    Train the model with the prototypical learning algorithm
+    '''
+
+    device = 'cuda:0' if torch.cuda.is_available() and opt.cuda else 'cpu'
+    loss_fn = prototypical_loss
+
+    for epoch in range(opt.epochs):
+        print('=== Epoch: {} ==='.format(epoch))
+        acc_statistic = []
+        loss_statistic = []
+
+        tr_iter = iter(tr_dataloader)
+        model.train()
+        for batch in tqdm(tr_iter):
+            optim.zero_grad()
+            x, y = batch  # 应该可以用吧？
+            x, y = x.to(device), y.to(device)
+            model_output = model(x)
+            loss, acc = loss_fn(model_output, target=y,
+                                n_support=opt.num_support_tr)
+            loss.backward()
+            optim.step()
+
+            acc_statistic.append(acc.item())
+            loss_statistic.append(loss.item())
+
+        print('Average Train Loss: {}, Avg Train Accuracy: {}'
+              .format(mean(loss_statistic), mean(acc_statistic)),
+              end='\n\n')
+
+        torch.save(
+            model.state_dict(),
+            os.path.join(opt.experiment_root, 'model_{}.pth'.format(epoch))
+        )
+
+        lr_scheduler.step()
+
+    return 0
+
+
+def initialize(options):
+    '''
+    初始化训练所需的模型及数据集
+    '''
+    if not os.path.exists(options.experiment_root):
+        os.makedirs(options.experiment_root)
+    if torch.cuda.is_available() and not options.cuda:
+        print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+
+    init_seed(options)
+    tr_dataloader = init_dataloader(options, 'train')
+    model = ProtoNet().to(device)
+    optim = torch.optim.Adam(params=model.parameters(),
+                             lr=options.learning_rate)
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optim,
+                                                   gamma=options.lr_scheduler_gamma,
+                                                   step_size=options.lr_scheduler_step)
+
+    return tr_dataloader, model, optim, lr_scheduler
+
+
+def main():
+    options = get_parser().parse_args()
+
+    tr_dataloader, model, optim, lr_scheduler = initialize(options)
+    for batch in tr_dataloader:
+        x, y = batch
+        print(x.shape, y.shape)
+        print(Counter(y.numpy()))
+        print(len(Counter(y.numpy())))
+    # train(
+    #     opt=options,
+    #     tr_dataloader=tr_dataloader,
+    #     val_dataloader=None,
+    #     model=model,
+    #     optim=optim,
+    #     lr_scheduler=lr_scheduler
+    # )
+
+    # 加载训练好的model
+    device = 'cuda:0' if torch.cuda.is_available() and options.cuda else 'cpu'
+    model = ProtoNet().to(device)
+    for epoch in range(options.epochs):
+        print("===========model No.{}===========".format(epoch))
+        # model.load_state_dict(
+        #     torch.load("./model_save/model_{}.pth".format(epoch))
+        # )
+        # test_model(options, model,n_classes = n_classes)
+        raise NotImplementedError
+
+
+if __name__ == '__main__':
+    main()
