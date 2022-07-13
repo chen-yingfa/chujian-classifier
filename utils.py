9833909e5a3843659517f5feff71c267f8e0229d
Donny Chan <chenyingfa1999@qq.com>
2022-07-13 20:20:04 +0000

    Format code and cleanup

---
+++ utils.py
@@ -0,0 +1,21 @@
+from torchvision import transforms as T
+import numpy as np
+import torch
+
+
+def mean(arrs):
+    return sum(arrs) * 1.0 / len(arrs)
+
+
+def set_seed(opt):
+    np.random.seed(opt.manual_seed)
+    torch.manual_seed(opt.manual_seed)
+    torch.cuda.manual_seed(opt.manual_seed)
+
+
+transform = T.Compose([
+    T.Resize([50, 50]),
+    # T.Grayscale(),
+    T.ToTensor(),
+    T.Normalize(mean=[.5, .5, .5], std=[.5, .5, .5])
+])
