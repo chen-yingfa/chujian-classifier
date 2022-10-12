53501005e257ee63268414d9dd7d6c194c8d9f77
cyf <chenyingfa1999@qq.com>
2022-10-12 15:04:19 +0800

    add training for resnet

--- train_resnet.py
+++ train_resnet.py
@@ -2,29 +2,55 @@ from pathlib import Path
 
 import torch
 from torch import nn, Tensor
+from torchvision import transforms
 
-from data import ChujianDataset
+from dataset import ChujianDataset
+from trainer import Trainer
 
 
 def load_model() -> nn.Module:
-    model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
+    model = torch.hub.load(
+        'pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
     return model
 
 
 def train(
-    model, 
+    model,
     train_data: ChujianDataset,
 ):
     pass
+
 
 def main():
     output_dir = Path('result/chujian_955')
     train_dir = Path('data/chujian/glyphs_955_train')
     test_dir = Path('data/chujian/glyphs_955_test')
-    
-    dataset = ChujianDataset(output_dir / 'glyphs_955_train')
+    img_size = (50, 50)
+    train_transform = transforms.Compose([
+        transforms.Resize(img_size),
+        transforms.ToTensor(),
+        # TODO: Add more transformations: data augmentation, normalize etc.
+        transforms.GaussianBlur(kernel_size=3),
+        transforms.RandomResizedCrop(img_size),
+        # transforms.RandomPosterize(bits=2),
+        transforms.RandomAdjustSharpness(sharpness_factor=4),
+        transforms.RandomInvert(),
+        transforms.RandomAutocontrast(),
+        transforms.RandomGrayscale(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+    ])
+    test_transform = transforms.Compose([
+        transforms.Resize(img_size),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+    ])
+
+    train_data = ChujianDataset(train_dir, train_transform, True)
+    test_data = ChujianDataset(test_dir, test_transform, False)
+
     model = load_model()
-    train(model, dataset)
+    trainer = Trainer(model, output_dir, 2, 4, 0.001)
+    trainer.train(train_data, test_data)
 
 
 if __name__ == '__main__':
--- trainer.py
+++ trainer.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 import torch
-from torch import nn, Tensor
+from torch import nn
 from torch.utils.data import DataLoader, Dataset
 
 
@@ -18,12 +18,47 @@ class Trainer:
         self.output_dir = output_dir
         self.num_epochs = num_epochs
         self.batch_size = batch_size
-        
-        
+
+    def _train_epoch(self, train_loader: DataLoader):
+        self.model.train()
+        for batch in train_loader:
+            self._train_batch(batch)
+
+    def _train_batch(self, batch):
+        image, label = batch
+        outputs = self.model(image)
+        print(outputs)
+        exit()
+
     def train(
+        self,
         train_data: Dataset,
         dev_data: Dataset,
     ):
         train_loader = DataLoader(train_data, batch_size=32, shuffle=True)
-        
-    
+        self.cur_ep = 0
+
+        while self.cur_ep < self.num_epochs:
+            self._train_epoch(train_loader)
+            self.cur_ep += 1
+            self.save_ckpt(f'ckpt_{self.cur_ep}.pt')
+
+    def save_ckpt(self, filename: str):
+        torch.save(self.model.state_dict(), self.output_dir / filename)
+
+    def load_ckpt(self, filename: str) -> nn.Module:
+        sd = torch.load(self.output_dir / filename)
+        self.model.load_state_dict(sd)
+
+    def evaluate(self, dataset):
+        loader = DataLoader(dataset, batch_size=32, shuffle=False)
+        self.model.eval()
+        with torch.no_grad():
+            for batch in loader:
+                self.eval_step(batch)
+
+    def eval_step(self, batch):
+        image, label = batch
+        outputs = self.model(image)
+        print(outputs)
+        exit()
