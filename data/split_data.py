fb3429e7df0c105c8a343260e7f3cb37745665ea
cyf <chenyingfa1999@qq.com>
2022-10-12 14:42:30 +0800

    update

--- data/split_data.py
+++ data/split_data.py
@@ -6,9 +6,9 @@ import random
 # data_dir = Path('./chujian/glyphs_small')
 # train_dir = Path('./chujian/glyphs_small_train')
 # test_dir = Path('./chujian/glyphs_small_test')
-data_dir = Path('./chujian/glyphs')
-train_dir = Path('./chujian/glyphs_train')
-test_dir = Path('./chujian/glyphs_test')
+data_dir = Path('./chujian/glyphs_955')
+train_dir = Path('./chujian/glyphs_955_train')
+test_dir = Path('./chujian/glyphs_955_test')
 
 train_dir.mkdir(exist_ok=True, parents=True)
 test_dir.mkdir(exist_ok=True, parents=True)
@@ -23,30 +23,40 @@ for glyph_dir in data_dir.iterdir():
     test_glyph_dir.mkdir(exist_ok=True, parents=True)
 
 print('Looping image files...')
-images = []
+images = {}
 for glyph_dir in data_dir.iterdir():
+    glyph = glyph_dir.name
+    images[glyph] = []
     for image in glyph_dir.iterdir():
-        images.append(image)
+        images[glyph].append(image)
 
-# Split images into train and test.
-split_idx = int(len(images) * 0.8)
-random.shuffle(images)
-train_images = images[:split_idx]
-test_images = images[split_idx:]
+# Split into train and test set
+# For each class, split by 4:1 ratio.
+train_images = {}
+test_images = {}
+for glyph, image_files in images.items():
+    random.seed(0)
+    random.shuffle(image_files)
+    split_idx = int(len(image_files) * 0.8)
+    train_images[glyph] = image_files[:split_idx]
+    test_images[glyph] = image_files[split_idx:]
 
 print('Train images:', len(train_images))
 print('Test images:', len(test_images))
 
 # Copy images to train and test.
 print('Copying train images...')
-for image in train_images:
-    dst_file = train_dir / image.parent.name / image.name
-    dst_file.parent.mkdir(exist_ok=True, parents=True)
-    copyfile(image, dst_file)
+for glyph, images in train_images.items():
+    for image in images:
+        dst_file = train_dir / glyph / image.name
+        dst_file.parent.mkdir(exist_ok=True, parents=True)
+        copyfile(image, dst_file)
 
 
 print('Copying test images...')
-for image in test_images:
-    dst_file = test_dir / image.parent.name / image.name
-    dst_file.parent.mkdir(exist_ok=True, parents=True)
-    copyfile(image, dst_file)
+
+for glyph, images in test_images.items():
+    for image in images:
+        dst_file = test_dir / glyph / image.name
+        dst_file.parent.mkdir(exist_ok=True, parents=True)
+        copyfile(image, dst_file)
